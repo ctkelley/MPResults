b3ab6c24649178ff13d944c541d39ddfb1474bfa
Tim Kelley <ctk@ncsu.edu>
2020-05-18 15:49:20 -0400

    Fresh data

---
+++ Data_From_Paper/MP_Data_c=1/plot_c1_2.1.jl
@@ -0,0 +1,9 @@
+#include("precision64.jl")
+#include("precision64fd.jl")
+#include("precision32.jl")
+#include("precision32fd.jl")
+
+precision64(30)
+precision64fd(30)
+precision32(30)
+precision32fd(30)
