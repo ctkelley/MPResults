4ecadecdba6efe0133f8a8e3d96acef9e51490a7
Tim Kelley <ctk@ncsu.edu>
2020-06-13 13:52:19 -0400

    JLD2 --> JLD

--- src/convertjld.jl
+++ src/convertjld.jl
@@ -3,7 +3,7 @@ This thing converts the old JLD2 stuff into the more stable JLD.
 
 When I find the time I put the whole works into csv for long-term stability.
 
-You must use this from the REPL and to 
+You must use this from the REPL and do 
 
 import JLD2
 import JLD
@@ -14,8 +14,8 @@ before you start.
 function convertjld()
 oldfiles=readdir()
 for fold in oldfiles
-    fname=findfirst('j',fz) 
-    if typeof(fname) != nothing
+    fname=findfirst('j',fold) 
+    if typeof(fname) != Nothing
         fnew=string(fold[1:fname-1],"jld")
         @JLD2.load fold fout32 fout64
         @JLD.save fnew fout32 fout64
@@ -26,8 +26,8 @@ end
 function convert16jld()
 oldfiles=readdir()
 for fold in oldfiles
-    fname=findfirst('j',fz)
-    if typeof(fname) != nothing
+    fname=findfirst('j',fold)
+    if typeof(fname) != Nothing
         fnew=string(fold[1:fname-1],"jld")
         @JLD2.load fold fout16
         @JLD.save fnew fout16
